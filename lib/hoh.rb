087d96f7fb4a8d35bdafd5eb94e5bd0e68e54c33
Valentin Placido <valentinplacido25@gmail.com>
2020-01-17 04:48:09 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -37,7 +37,10 @@ def naming_system
      sub_category: {
        label: "Phylum",
        sub_category: {
-         label: "Class"
+         label: "Class", 
+         sub_category: {
+           label: "Order"
+         }
        }
      }
    }
